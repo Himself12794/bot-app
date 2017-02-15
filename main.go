fa0c5d544eb387617b609b64eb2fc8ebe6c73f7b
phwhitin <phwhitin@cisco.com>
2017-02-15 15:16:31 -0500

    initial commit

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+    "bytes"
+    "io/ioutil"
+)
+
+const (
+    apiURL = "https://api.ciscospark.com/v1/messages"
+    testRoom = "Y2lzY29zcGFyazovL3VzL1JPT00vNWZmNGM1ZTAtZjNhMS0xMWU2LWJhOWYtOTUwN2UyMTZkOTRj"
+    testToken = "MzVhMzc3NzYtNDNjYS00MWZkLWJjODgtN2JjMWIwNzgzYTY4YjMwZjE4MGMtNGFj"
+    message = "Test message"
+)
+
+func main() {
+    sendTestMessage()
+}
+
+func sendTestMessage() {
+    
+    fmt.Println("URL:>", apiURL)
+
+    var jsonStr = []byte(`{"markdown":"` + message + `", "roomId":"` + testRoom + `"}`)
+                     
+    req, err := http.NewRequest("POST", apiURL, bytes.NewBuffer(jsonStr))
+    req.Header.Set("Authorization", "Bearer " + testToken)
+    req.Header.Set("Content-Type", "application/json")
+
+    client := &http.Client{}
+    resp, err := client.Do(req)
+    if err != nil {
+        panic(err)
+    }
+    defer resp.Body.Close()
+
+    fmt.Println("response Status:", resp.Status)
+    fmt.Println("response Headers:", resp.Header)
+    body, _ := ioutil.ReadAll(resp.Body)
+    fmt.Println("response Body:", string(body))
+}
