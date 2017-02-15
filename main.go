2f99d6086550dcaa7411f45fe193573ac4a93319
phwhitin <phwhitin@cisco.com>
2017-02-15 15:37:41 -0500

    added flag

--- main.go
+++ main.go
@@ -12,8 +12,9 @@ const (
     apiURL = "https://api.ciscospark.com/v1/messages"
     testRoom = "Y2lzY29zcGFyazovL3VzL1JPT00vNWZmNGM1ZTAtZjNhMS0xMWU2LWJhOWYtOTUwN2UyMTZkOTRj"
     testToken = "MzVhMzc3NzYtNDNjYS00MWZkLWJjODgtN2JjMWIwNzgzYTY4YjMwZjE4MGMtNGFj"
-    message = flag.String("msg", "Test", "The message to send to Spark")
 )
+
+var message = flag.String("msg", "Test", "The message to send to Spark")
 
 func main() {
     sendTestMessage()
